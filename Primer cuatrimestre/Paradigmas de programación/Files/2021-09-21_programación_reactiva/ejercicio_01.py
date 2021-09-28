0da23cf34d4ec1b69e35f095e91749cb000495a5
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2021-09-28 15:23:34 +0200

    Arreglado ejercicio1

--- Primer cuatrimestre/Paradigmas de programación/Files/2021-09-21_programación_reactiva/ejercicio_01.py
+++ Primer cuatrimestre/Paradigmas de programación/Files/2021-09-21_programación_reactiva/ejercicio_01.py
@@ -1,37 +1,22 @@
 import requests
-from rx import create
+from rx import from_, operators
 from rx.core import Observer
 
 API_TOKEN = 'lmao-no'
 
 
 class FilmRenderer(Observer):
-    def on_next(self, film: dict) -> None:
-        if film['Type'] != 'movie':
-            return
-        print(f'{film["imdbID"][2:]} - {film["Title"]}: {film["Poster"]}')
-
-    def on_completed(self) -> None:
-        print('Fin!')
+    def on_next(self, film: str) -> None:
+        print(film)
 
 
-def observer_teclado(observer, scheduler):
-    while True:
-        title = input('Introduce un título para buscar: ')
-        if not title:
-            break
+params = {
+    'apikey': API_TOKEN,
+    's': input('Introduce un título para buscar: ')
+}
+content = requests.get('http://www.omdbapi.com/', params=params).json()
 
-        params = {'apikey': API_TOKEN, 't': title}
-        content = requests.get('http://www.omdbapi.com/', params=params).json()
-
-        if isinstance(content, list):
-            for film in content['Search']:
-                observer.on_next(film)
-        else:
-            observer.on_next(content)
-
-        observer.on_completed()
-
-
-observable = create(observer_teclado)
-observable.subscribe(FilmRenderer())
+from_(content['Search']).pipe(
+    operators.filter(lambda f: f['Type'] == 'movie'),
+    operators.map(lambda f: f'{f["imdbID"][2:]} - {f["Title"]}: {f["Poster"]}'),
+).subscribe(FilmRenderer())
